2ddea95fcff91be698456ba23ee90d1eac210e66
wes1993 <52070589+wes1993@users.noreply.github.com>
2025-06-26 11:34:34 +0200

    Add Timestamp in Device Attributes to know last updated state in Apple Servers

--- custom_components/findmy/device_tracker.py
+++ custom_components/findmy/device_tracker.py
@@ -6,6 +6,7 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
+from homeassistant.core import HomeAssistant, callback  # <<< qui aggiunto
 
 from . import FindMyConfigEntry
 from .coordinator import FindMyUpdateCoordinator
@@ -34,6 +35,12 @@ class FindMyDeviceTracker(CoordinatorEntity[FindMyUpdateCoordinator], TrackerEnt
         self._attr_name = coordinator.hub.accessory.name
         self._attr_source_type = SourceType.GPS
 
+        # Inizializza attributi extra (qui aggiungiamo il timestamp)
+        self._attr_extra_state_attributes = {
+            "timestamp": getattr(coordinator.data, "timestamp", None)
+        }
+
+
     @property
     def location_accuracy(self):
         """Return the location accuracy of the device."""
@@ -48,3 +55,14 @@ class FindMyDeviceTracker(CoordinatorEntity[FindMyUpdateCoordinator], TrackerEnt
     def longitude(self):
         """Return longitude value of the device."""
         return self.coordinator.data.longitude
+
+    @callback
+    def _handle_coordinator_update(self) -> None:
+        """Aggiorna l'entità quando il coordinatore riceve nuovi dati."""
+        # Aggiorna il timestamp negli extra attributes
+        self._attr_extra_state_attributes["timestamp"] = getattr(
+            self.coordinator.data, "timestamp", None
+        )
+
+        # Chiamata a super per aggiornare lo stato
+        super()._handle_coordinator_update()
